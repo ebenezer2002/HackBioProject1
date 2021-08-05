f687f355c2c0020f0294a845a4fc12009e6eb06d
Yukesh30 <84978196+Yukesh30@users.noreply.github.com>
2021-08-05 12:46:12 +0530

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -3,4 +3,5 @@ Email = "yukeswaran.bt19@bitsathy.ac.in"
 Slack_username = "@yukesh"
 Biostack = "drug development"
 Twitter_Handle = "@yukeswaran30"
-print(Name,',',Email,',',Slack_username,',',Biostack,',',Twitter_Handle)
+hamming_disatance = "7"
+print(Name,',',Email,',',Slack_username,',',Biostack,',',Twitter_Handle,',',hamming_disatance)
