4db0e75f20443d5eb530ab40dc24d701b0ab09e6
Odara Rapheal <78569972+odara-rapheal@users.noreply.github.com>
2021-08-06 21:33:39 +0100

    Update odara_script.py

--- odara_script.py
+++ odara_script.py
@@ -11,13 +11,13 @@ def hammingDistance(str1, str2):
     return count
 
 # defining variables
-name = "Odara Rapheal"
-email = "odararapheal@gmail.com"
-slack_username = "@odararaffy"
-biostack = "Data Science"
-twitter_handle = "@i_am_i_dra"
+name = "Odara Rapheal,"
+email = "odararapheal@gmail.com,"
+slack_username = "@odararaffy,"
+biostack = "Data Science,"
+twitter_handle = "@i_am_i_dra,"
 hamming_distance = hammingDistance(slack_username, twitter_handle)
 
 #printing the desired output
-print(name + "," + email + "," + slack_username + "," + biostack + "," + twitter_handle + "," +str(hamming_distance))
+print(name + email + slack_username + biostack + twitter_handle + str(hamming_distance))
 input()
